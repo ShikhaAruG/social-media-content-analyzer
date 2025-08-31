faf6730910a9bb6a621b2d0a710ecd8359282055
ShikhaAruG <2k22.cse.2211339@gmail.com>
2025-08-31 19:57:01 +0530

    Update render-build.sh

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -1,12 +1,3 @@
 #!/usr/bin/env bash
-# Exit if any command fails
-set -o errexit
-
-# Install Python dependencies
-pip install --upgrade pip
-pip install -r backend/requirements.txt
-
-# (Optional) If textstat fails, force install wheel
-pip install --no-cache-dir textstat
-
-# Install Tesseract if not installed (Render auto-detects apt.txt)
+apt-get update && apt-get install -y tesseract-ocr libtesseract-dev
+pip install -r requirements.txt
